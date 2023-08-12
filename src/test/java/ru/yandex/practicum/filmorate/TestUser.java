f81c1ef8213b37f997a82c7a99b2cf700e9c4585
SergeyNikiforov1990 <SergeyNikiforov32@yandex.ru>
2023-08-12 22:26:58 +0500

    controllers-films-users добавлены лайки и друзья

--- src/test/java/ru/yandex/practicum/filmorate/TestUser.java
+++ src/test/java/ru/yandex/practicum/filmorate/TestUser.java
@@ -3,13 +3,14 @@ package ru.yandex.practicum.filmorate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.UserController;
-import ru.yandex.practicum.filmorate.exception.ValidationException;   
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.service.UserServiceImpl;
 import ru.yandex.practicum.filmorate.storage.InMemoryUserStorage;
 
 import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
