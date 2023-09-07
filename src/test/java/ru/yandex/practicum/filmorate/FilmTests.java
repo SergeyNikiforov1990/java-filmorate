78dabd923b0f71b67f626be362d1e34c63dac39b
SergeyNikiforov1990 <SergeyNikiforov32@yandex.ru>
2023-09-07 12:17:12 +0500

    add-database 1

--- src/test/java/ru/yandex/practicum/filmorate/FilmTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmTests.java
@@ -17,7 +17,6 @@ import ru.yandex.practicum.filmorate.service.UserService;
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
